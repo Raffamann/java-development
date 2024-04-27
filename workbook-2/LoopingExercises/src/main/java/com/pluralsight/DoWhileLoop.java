0a13aaa3ba002204d46d90f14f0dc6afc2de5238
Raffamann <rafael.millaremanan@gmail.com>
2024-04-27 15:22:57 -0400

    LoopExercises

---
+++ workbook-2/LoopingExercises/src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 7;
+          do {
+              System.out.println("I'm ok at Java ");
+            i--;
+        } while (i > 0);
+    }
+}
+
---
+++ workbook-2/LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i > 0) {
+            System.out.println("I'm ok at Java ");
+            i --;
+        }
+    }
+}
